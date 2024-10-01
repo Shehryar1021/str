766243da2d6ecdfdda2ed217803922930c212b83
Shehryar1021 <kiranimtiaz1985@gmail.com>
2024-10-01 16:20:28 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+#input a word
+text =str(input("ENTER A STRING :"))
+
+# Reverse String
+# using step value as -1 to iterate in reverse
+revtext = text[::-1]
+text= revtext
+print("REVERSE OF GIVEN STRING IS")
+print(text)
